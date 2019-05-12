8b40d54d4844bacf900f048686184a8ca75687a7
lcd <853890474@qq.com>
2019-05-12 12:24:33 +0800

    class Square

---
+++ src/main/java/moe/jsteward/Math/sobol.java
@@ -0,0 +1,32 @@
+package moe.jsteward.Math;
+
+public class sobol {
+    /*
+     *  What is the initial value?
+     */
+
+    static final int num_dimensions = 0;
+    static final int size = 0;
+    static final long matrices[] = {0};
+
+    /*
+     *  calculate the sobol sequence
+     */
+    public double sample(long index, int dimension, long scramble) {
+
+        long result = scramble & ~-(1 << size);
+        for (int i = dimension * size; index > 0; index >>= 1, ++i) {
+            if ((index & 1) > 0)
+                result ^= matrices[i];
+        }
+
+        return result * (1.0 / (1 << size));
+    }
+
+    /*
+     *  calculate the sobol sequence
+     */
+    public double sample(long index, int dimension) {
+        return sample(index, dimension, 0);
+    }
+}
