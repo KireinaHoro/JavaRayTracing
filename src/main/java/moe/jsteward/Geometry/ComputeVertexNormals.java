38f1e14dbbba9befffc46238ba308b72911ad331
CloudLiu <liuyunhui2009910@hotmail.com>
2019-05-17 09:24:16 +0800

    computeVertexNormal fixed. confusing parts are tagged TODO.

--- src/main/java/moe/jsteward/Geometry/ComputeVertexNormals.java
+++ src/main/java/moe/jsteward/Geometry/ComputeVertexNormals.java
@@ -54,6 +54,109 @@ class ComputeVertexNormals {
     }
 
     /*
+     * Computes the edge to triangle map
+     */
+    private void computeEdgeToTriangle() {
+        m_edgeToTriangle = new HashMap<>();
+        for (int cpt = 0; cpt < m_triangles.size(); ++cpt) {
+            Triangle triangle = m_triangles.elementAt(cpt);
+            for (int i = 0; i < 3; ++i) {
+                MutablePair<Vector3D, Vector3D> edge = makeEdge(triangle.vertex(i), triangle.vertex((i + 1) % 3));
+                if (!m_edgeToTriangle.containsKey(edge))
+                    m_edgeToTriangle.put(edge, new Vector<>());
+                // TODO Cloud : I doubt if this works...
+                m_edgeToTriangle.get(edge).add(cpt);
+            }
+        }
+    }
+
+    /*
+     * Filters the edge to triangle Map by removing edges shared by triangles
+     * for which the dot product of normals is lower than the given threshold
+     */
+    private void filterEdges(double cosLimit) {
+        Vector<Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>>> toRemove = new Vector<>();
+        Vector<Integer> trianglesToRemove = new Vector<>();
+        Iterator<Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>>> it = m_edgeToTriangle.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>> entry = it.next();
+            Vector<Integer> triangles = entry.getValue();
+            if (triangles.size() < 2) {
+                toRemove.add(entry);
+            } else if (triangles.size() == 2) {
+                if (m_triangles.elementAt(triangles.elementAt(0)).normal().dotProduct(m_triangles.elementAt(triangles.elementAt(1)).normal()) < cosLimit) {
+                    toRemove.add(entry);
+                }
+            } else {
+                toRemove.add(entry);
+                for (Vector<Integer> vec : m_edgeToTriangle.values())
+                    trianglesToRemove.addAll(vec);
+            }
+        }
+        for (Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>> it1 : toRemove) {
+            // TODO Cloud : i saw the warning... confusing...
+            // TODO changed by simply dealing with the warning
+            // TODO because it has two delete parts, hard to change...
+            m_edgeToTriangle.remove(it1.getKey(), it1.getValue());
+        }
+        toRemove.clear();
+        Collections.sort(trianglesToRemove);
+        Set<Integer> tempset = new HashSet<>(trianglesToRemove);
+        trianglesToRemove = new Vector<>(tempset);
+        it = m_edgeToTriangle.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>> entry = it.next();
+            if (Arrays.binarySearch(trianglesToRemove.toArray(), entry.getValue().elementAt(0)) != -1) {
+                toRemove.add(entry);
+            } else if (Arrays.binarySearch(trianglesToRemove.toArray(), entry.getValue().elementAt(1)) != -1) {
+                toRemove.add(entry);
+            }
+        }
+        for (Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>> it1 : toRemove) {
+            m_edgeToTriangle.remove(it1.getKey(), it1.getValue());
+        }
+    }
+
+    /*
+     * Computes the groups of triangles based on the edge to triangle Map
+     * Pointer need todo
+     */
+    private void computeGroups() {
+        m_groups = new Vector<>();
+        // 1- We compute the triangle adjency graph.
+        Vector<Vector<Integer>> graph = new Vector<>(m_triangles.size());
+        for (Vector<Integer> it : m_edgeToTriangle.values()) {
+            // TODO CLoud: i saw this, does it change graph elements?
+            graph.elementAt(it.elementAt(1)).add(it.elementAt(1));
+            graph.elementAt(it.elementAt(0)).add(it.elementAt(0));
+        }
+
+        // 2 - We compute the connected components.
+        Vector<Boolean> explored = new Vector<>(m_triangles.size());/* maybe need init to false */
+        for (int i = 0; i < m_triangles.size(); ++i) {
+            explored.setElementAt(false, i);
+        }
+        Vector<Integer> toExplore = new Vector<>();
+        for (int cpt = 0; cpt < graph.size(); ++cpt) {
+            if (explored.elementAt(cpt)) {
+                continue;
+            }
+            m_groups.add(new Vector<>());
+            toExplore.add(cpt);
+            while (!toExplore.isEmpty()) {
+                int current = toExplore.lastElement();
+                toExplore.remove(toExplore.lastElement());/*pop_back() */
+                if (explored.elementAt(current)) {
+                    continue;
+                }
+                explored.setElementAt(true, current);
+                m_groups.lastElement().add(current);
+                toExplore.addAll(graph.elementAt(current));
+            }
+        }
+    }
+
+    /*
      * auto need todo
      */
     private void computeNormalsForGroups() {
@@ -86,100 +189,11 @@ class ComputeVertexNormals {
                     triangle.setVertexNormal(i, found.normalize());
                 }
             }
+            m_triangles.setElementAt(triangle, it);
         }
     }
 
-    /*
-     * Computes the groups of triangles based on the edge to triangle Map
-     * Pointer need todo
-     */
-    private void computeGroups() {
-        // 1- We compute the triangle adjency graph.
-        Vector<Vector<Integer>> graph = new Vector<>(m_triangles.size());
-        for (Vector<Integer> it : m_edgeToTriangle.values()) {
-            graph.elementAt(it.elementAt(1)).add(it.elementAt(1));
-            graph.elementAt(it.elementAt(0)).add(it.elementAt(0));
-        }
 
-        // 2 - We compute the connected components.
-        Vector<Boolean> explored = new Vector<>(m_triangles.size());/* maybe need init to false */
-        Vector<Integer> toExplore = new Vector<>();
-        for (int cpt = 0; cpt < graph.size(); ++cpt) {
-            if (explored.elementAt(cpt)) {
-                continue;
-            }
-            m_groups.add(new Vector<>());
-            toExplore.add(cpt);
-            while (!toExplore.isEmpty()) {
-                int current = toExplore.lastElement();
-                toExplore.remove(toExplore.lastElement());/*pop_back() */
-                if (explored.elementAt(current)) {
-                    continue;
-                }
-                explored.setElementAt(true, current);
-                m_groups.lastElement().add(current);
-                toExplore.addAll(graph.elementAt(current));
-            }
-        }
-    }
-
-    /*
-     * Computes the edge to triangle map
-     */
-    private void computeEdgeToTriangle() {
-        for (int cpt = 0; cpt < m_triangles.size(); ++cpt) {
-            Triangle triangle = m_triangles.elementAt(cpt);
-            for (int i = 0; i < 3; ++i) {
-                m_edgeToTriangle.get(makeEdge(triangle.vertex(i), triangle.vertex((i + 1) % 3))).add(cpt);
-            }
-        }
-    }
-
-    /*
-     * Filters the edge to triangle Map by removing edges shared by triangles for which the dot product
-     * of normals is lower than the given threshold
-     */
-
-    private void filterEdges(double cosLimit) {
-        Vector<Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>>> toRemove = new Vector<>();
-        Vector<Integer> trianglesToRemove = new Vector<>();
-        Iterator<Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>>> it = m_edgeToTriangle.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>> entry = it.next();
-            Vector<Integer> triangles = entry.getValue();
-            if (triangles.size() < 2) {
-                toRemove.add(entry);
-            } else if (triangles.size() == 2) {
-                if (m_triangles.elementAt(triangles.elementAt(0)).normal().dotProduct(m_triangles.elementAt(triangles.elementAt(1)).normal()) < cosLimit) {
-                    toRemove.add(entry);
-                }
-            } else {
-                toRemove.add(entry);
-                for (Vector<Integer> vec : m_edgeToTriangle.values())
-                    trianglesToRemove.addAll(vec);
-            }
-        }
-        for (Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>> it1 : toRemove) {
-            // TODO Cloud : i saw the warning... confusing...
-            m_edgeToTriangle.remove(it1);
-        }
-        toRemove.clear();
-        Collections.sort(trianglesToRemove);
-        Set<Integer> tempset = new HashSet<>(trianglesToRemove);
-        trianglesToRemove = new Vector<>(tempset);
-        it = m_edgeToTriangle.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>> entry = it.next();
-            if (Arrays.binarySearch(trianglesToRemove.toArray(), entry.getValue().elementAt(0)) != -1) {
-                toRemove.add(entry);
-            } else if (Arrays.binarySearch(trianglesToRemove.toArray(), entry.getValue().elementAt(1)) != -1) {
-                toRemove.add(entry);
-            }
-        }
-        for (Map.Entry<MutablePair<Vector3D, Vector3D>, Vector<Integer>> it1 : toRemove) {
-            m_edgeToTriangle.remove(it1);
-        }
-    }
 
     /*
      * Ensures that the cosine of the angle between normals of each triangle is greater than the given threshold
